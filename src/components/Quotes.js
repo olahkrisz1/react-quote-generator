e108318de2d2d0e6d3ece00fe65a4b7ec8431614
olahkrisz1 <olahkrisz1@gmail.com>
2023-06-23 21:40:53 +0200

    quotes pages completed

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const Quotes = () => {
+ const [quote, setQuote] = useState('');
+ const [author, setAuthor] = useState('');
+
+ useEffect(() => {
+    getQuote()
+ }, [])
+
+ const getQuote = () => {
+    let url = 'https://type.fit/api/quotes';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        let randomNum = Math.floor(Math.random() * data.length);
+        let randomQuote = data[randomNum];
+        setQuote(randomQuote.text);
+        setAuthor(randomQuote.author);
+      })
+      .catch(error => {
+        console.log('Error fetching quotes:', error);
+      });
+  };
+
+  const handleClick = () => {
+    getQuote()
+  }
+
 return (
 <div id="quote-box">
     <div id="text">
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ipsam facere nobis quidem minus corporis
-            incidunt
-            sit nesciunt non libero?
+            {quote}
         </p>
         <br />
         <div id="author">
-            <p>John Doe</p>
+            <p>{author}</p>
         </div>
         <div id="buttons">
-            <div className="social-media">
-                <a href="#" id="tweet-quote">
-                    <span><img src="" alt="" /></span>
-                </a>
-                <a href="#" id="tumlr-quote">
-                    <span><img src="" alt="" /></span>
-                </a>
-            </div>
-            <button id="new-quote">New Quote</button>
+            <button onClick={handleClick} id="new-quote">New Quote</button>
         </div>
     </div>
 </div>
